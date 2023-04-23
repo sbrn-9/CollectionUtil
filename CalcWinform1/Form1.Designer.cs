b04a32037048ea91e9fa4f692c4a6bb2b086f82b
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-22 23:19:32 -0300

    Add files via upload

---
+++ CalcWinform1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CalcWinform1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            lblEstado = new Label();
+            btnGenerate = new Button();
+            panelCalc = new Panel();
+            btnRemover = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(33, 26);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.MouseMove += button1_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // lblEstado
+            // 
+            lblEstado.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            lblEstado.AutoSize = true;
+            lblEstado.BackColor = Color.NavajoWhite;
+            lblEstado.Location = new Point(2, 435);
+            lblEstado.Name = "lblEstado";
+            lblEstado.Size = new Size(38, 15);
+            lblEstado.TabIndex = 2;
+            lblEstado.Text = "label2";
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(208, 26);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(278, 32);
+            btnGenerate.TabIndex = 4;
+            btnGenerate.Text = "Generate";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // panelCalc
+            // 
+            panelCalc.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panelCalc.BackColor = Color.Cornsilk;
+            panelCalc.Location = new Point(33, 101);
+            panelCalc.Name = "panelCalc";
+            panelCalc.Size = new Size(734, 312);
+            panelCalc.TabIndex = 5;
+            // 
+            // btnRemover
+            // 
+            btnRemover.Location = new Point(501, 26);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(116, 32);
+            btnRemover.TabIndex = 6;
+            btnRemover.Text = "Remover";
+            btnRemover.UseVisualStyleBackColor = true;
+            btnRemover.Click += btnRemover_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnRemover);
+            Controls.Add(panelCalc);
+            Controls.Add(btnGenerate);
+            Controls.Add(lblEstado);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            Resize += Form1_Resize;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label lblEstado;
+        private Button btnGenerate;
+        private Panel panelCalc;
+        private Button btnRemover;
+    }
+}
---
+++ CalcWinform1/Form1.cs
@@ -0,0 +1,120 @@
+using Calc.Core.Interfaces;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace CalcWinform1
+{
+    public partial class Form1 : Form
+    {
+        private ICalc _calc;
+
+        private List<Button> lista;
+
+        public Form1(ICalc calc)
+        {
+            _calc = calc;
+
+            _calc.Procesando += Calc_Procesando_Demo;
+            _calc.Termino += Calc_Termino_Demo;
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var resultado = _calc.Add(1, 1);
+
+            label1.Text = resultado.ToString();
+        }
+
+        private void Calc_Procesando_Demo(object sender, EventArgs e)
+        {
+            lblEstado.Text = "Procesando....";
+        }
+
+        private void Calc_Termino_Demo(object sender, EventArgs e)
+        {
+            lblEstado.Text = "Termino!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            lblEstado.Text = $"Resize! {DateTime.Now.ToLongTimeString()}";
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEstado.Text = $"MouseMove! {DateTime.Now.ToLongTimeString()}";
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            //Se busca remover los botones usando CollectionUtil
+            Hashtable botones = CollectionsUtil.CreateCaseInsensitiveHashtable();
+            botones["Sumar"] = "Suma"; //Nombre: sumar Valor: Suma
+            botones["Restar"] = "Resta";
+            botones["Division"] = "Dividir";
+            botones["Multiplicacion"] = "Multiplicar";
+
+            var lista = new List<Button>();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                var b = new Button();
+                b.Text = i.ToString();
+
+                b.Location = new Point((i * 40), (i * 40));
+
+
+                panelCalc.Controls.Add(b);
+                lista.Add(b);
+            }
+
+            //Creación de los botones
+
+            var bAdd = new Button();
+            bAdd.Text = botones["SumAr"].ToString(); //Muestra el valor Suma
+
+            bAdd.Location = new Point(90, 0);
+            panelCalc.Controls.Add(bAdd);
+
+            lista.Add(bAdd);
+
+            var bResta = new Button();
+            bResta.Text = botones["RESTAr"].ToString(); //Muestra el valor Resta
+
+            bResta.Location = new Point(180, 0);
+            panelCalc.Controls.Add(bResta);
+
+            lista.Add(bResta);
+
+            var bMulti = new Button();
+            bMulti.Text = botones["MultIPlicacion"].ToString(); //Muestra el valor Multiplicar
+
+            bMulti.Location = new Point(180, 20);
+            panelCalc.Controls.Add(bMulti);
+
+            lista.Add(bMulti);
+
+            var bDiv = new Button();
+            bDiv.Text = botones["DivISion"].ToString(); //Muestra el valor Dividir
+
+            bDiv.Location = new Point(270, 40);
+            panelCalc.Controls.Add(bDiv);
+
+            lista.Add(bDiv);
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            //encontrar la forma de remover
+   
+
+        }
+    }
+}
