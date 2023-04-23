f2c1ade22ebde8b17ec30aadc279e468f377f38d
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-22 23:23:49 -0300

    Add files via upload

---
+++ Calculadora/CalcSuperpower/CalcSuperpower.cs
@@ -0,0 +1,32 @@
+﻿using Calc.Core.Interfaces;
+
+namespace CalcSuperpower
+{
+    public class CalcSuperpower : ICalc
+    {
+        public EventHandler Procesando { get; set; }
+        public EventHandler Termino { get; set; }
+
+
+        public int Add(int x, int y)
+        {
+            if (Procesando != null)
+            {
+                Procesando.Invoke(this, new EventArgs());
+            }
+
+            
+
+            var resultado = x + y + 10000;
+
+
+            if (Termino != null)
+            {
+                Termino.Invoke(this, new EventArgs());
+            }
+
+            return resultado;
+
+        }
+    }
+}
