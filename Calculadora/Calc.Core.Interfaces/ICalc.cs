7b1f390d8df3b8b6a185f003c7fd5dc26b461bee
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-22 23:25:58 -0300

    Add files via upload

---
+++ Calculadora/Calc.Core.Interfaces/ICalc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Core.Interfaces
+{
+    public interface ICalc
+    {
+        EventHandler Procesando { get; set; }
+        EventHandler Termino { get; set; }
+
+        int Add (int x, int y);
+    }
+}
