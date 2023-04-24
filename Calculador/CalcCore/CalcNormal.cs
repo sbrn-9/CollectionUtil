c8c5e03b8a171966bc64e2db25a5185c0b042f03
Sabrina <113538071+sbrn-9@users.noreply.github.com>
2023-04-24 11:55:16 -0300

    Add files via upload

---
+++ Calculador/CalcCore/CalcNormal.cs
@@ -0,0 +1,30 @@
+﻿using Calc.Core.Interfaces;
+
+namespace CalcCore
+{
+    public class CalcNormal : ICalc
+    {
+        public EventHandler Procesando { get; set; }
+        public EventHandler Termino { get; set; }
+
+        public int Add(int x, int y)
+        {
+            if (Procesando != null)
+            {
+                Procesando.Invoke(this, new EventArgs());
+            }
+            
+
+            var resultado = x + y;
+
+
+            if (Termino!=null)
+            {
+                Termino.Invoke(this, new EventArgs());
+            }
+           
+
+            return resultado;
+        }
+    }
+}
